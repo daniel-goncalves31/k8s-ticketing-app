3f4b71c9368c8500209f1b6d33d321c9b5414750
Daniel <danielgg0831@gmail.com>
2021-05-15 00:28:45 -0300

    feat: create current-user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,10 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+import { currentuser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentuser: null })
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-    return res.send({ currentuser: payload })
-  } catch (error) {
-    return res.send({ currentuser: null })
-  }
+router.get('/api/users/currentuser', currentuser, (req, res) => {
+  res.send({ currentuser: req.currentuser || null })
 })
 
 export { router as currentUserRouter }
