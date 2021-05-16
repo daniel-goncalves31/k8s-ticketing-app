c322b6d68ff583a4b55d6a19302b3370cb0d989d
Daniel <danielgg0831@gmail.com>
2021-05-15 22:59:35 -0300

    refactor: move the express app from it own file

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import cookieSession from 'cookie-session'
+import express from 'express'
+import 'express-async-errors'
+import { NotFoundError } from './errors'
+import { errorHandler } from './middlewares'
+import {
+  currentUserRouter,
+  signInUserRouter,
+  signOutUserRouter,
+  signUpUserRouter,
+} from './routes'
+
+const app = express()
+app.set('trust proxy', true)
+
+app.use(express.json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
+
+app.use(currentUserRouter)
+app.use(signInUserRouter)
+app.use(signOutUserRouter)
+app.use(signUpUserRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import cookieSession from 'cookie-session'
-import express from 'express'
-import 'express-async-errors'
 import mongoose from 'mongoose'
-import { NotFoundError } from './errors'
-import { errorHandler } from './middlewares'
-import {
-  currentUserRouter,
-  signInUserRouter,
-  signOutUserRouter,
-  signUpUserRouter,
-} from './routes'
-
-const app = express()
-app.set('trust proxy', true)
-
-app.use(express.json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-)
-
-app.use(currentUserRouter)
-app.use(signInUserRouter)
-app.use(signOutUserRouter)
-app.use(signUpUserRouter)
-
-app.all('*', async () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 const startDb = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY not defined')
